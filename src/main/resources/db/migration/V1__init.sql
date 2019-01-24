13e31728f24c895474282b34d46b14fb351b182d
nkhodask <sly.boogie22@gmail.com>
2019-01-25 00:30:12 +0530

    initial commit

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+  id bigint NOT NULL,
+  username varchar(100) NOT NULL,
+  first_name varchar(50) NOT NULL,
+  last_name varchar(50) DEFAULT NULL,
+  PRIMARY KEY (id)
+)
---
+++ src/main/resources/db/migration/V2__testdata.sql
@@ -0,0 +1,2 @@
+INSERT INTO users(id, username, first_name, last_name) VALUES(1, 'callicoder', 'Rajeev', 'Singh');
+INSERT INTO users(id, username, first_name, last_name) VALUES(2, 'flywaytest', 'Flyway', 'Test');
